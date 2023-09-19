003ed92c75ff3652ef4280bca23925e275316054
toofan <toofan.shian@gmail.com>
2023-09-19 17:02:19 +0330

    create a test to check if the form exists in component

--- tests/unit/Form.spec.js
+++ tests/unit/Form.spec.js
@@ -1,3 +1,10 @@
 import {mount} from '@vue/test-utils'
 import Form from '@/components/Form'
 
+describe("Form.vue", () => {
+
+  it('renders the form', () => {
+    let wrapper = mount(Form)
+    expect(wrapper.find('#form').exists()).toBe(true)
+  })
+})
