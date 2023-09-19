fa5286ce167baec54eeb146ec2388a34a2d55b6f
toofan <toofan.shian@gmail.com>
2023-09-19 18:14:49 +0330

    create the base form of search component and pass the search field existance test

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,13 +1,37 @@
 <template>
   <div>
-    search
+    <h3>Search Customers</h3>
+    <!-- Search input field -->
+    <input
+      id="search-field"
+      v-model="searchQuery"
+      placeholder="Search by name"
+      type="text"
+      class="search-input"
+    />
+
+    <!-- search results here -->
+    <ul class="search-results">
+      <li v-for="customer in searchResults" :key="customer.id" class="search-item">
+        {{ customer.firstName }} {{ customer.lastName }}
+      </li>
+    </ul>
   </div>
 </template>
 
-<script >
+<script>
 export default {
+  data() {
+    return {
+      searchQuery: '',
+      searchResults: [],
+    };
+  },
+  methods: {
 
-}
+
+  },
+};
 </script>
 
 <style scoped>
