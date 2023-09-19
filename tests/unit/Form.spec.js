982eba0929f7f8fede05d8a7cd6bc419a7ecc8b7
toofan <toofan.shian@gmail.com>
2023-09-19 17:14:05 +0330

    add test to check if all the form fields are there stat:pass

--- tests/unit/Form.spec.js
+++ tests/unit/Form.spec.js
@@ -7,4 +7,14 @@ describe("Form.vue", () => {
     let wrapper = mount(Form)
     expect(wrapper.find('#form').exists()).toBe(true)
   })
+
+  it('renders form inputs', () => {
+    const wrapper = mount(Form);
+    expect(wrapper.find('#firstName').exists()).toBe(true);
+    expect(wrapper.find('#lastName').exists()).toBe(true);
+    expect(wrapper.find('#dob').exists()).toBe(true);
+    expect(wrapper.find('#phoneNumber').exists()).toBe(true);
+    expect(wrapper.find('#email').exists()).toBe(true);
+    expect(wrapper.find('#bankAccount').exists()).toBe(true);
+  });
 })
