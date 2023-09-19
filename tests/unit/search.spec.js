fbdf2da60b7e27605386fdf179ab0c7ff6e2c8a1
toofan <toofan.shian@gmail.com>
2023-09-19 18:18:15 +0330

    add test to check if the search field shows the filtered customers

--- tests/unit/search.spec.js
+++ tests/unit/search.spec.js
@@ -8,4 +8,24 @@ describe('Search.vue', () => {
     let wrapper = mount(Search)
     expect(wrapper.get('#search-field'))
   })
+
+
+  it('displays matching customers when searching by name', async () => {
+    // Set up the initial data in local storage
+    localStorage.setItem('customerData', JSON.stringify([
+      { id: 1, firstName: 'John', lastName: 'Doe' },
+      { id: 2, firstName: 'Jane', lastName: 'Smith' },
+    ]));
+  
+    const wrapper = mount(Search);
+  
+    const input = wrapper.find('#search-field');
+  
+    await input.setValue('John');
+  
+    await wrapper.vm.$nextTick();
+  
+    const searchResults = wrapper.findAll('ul li');
+    expect(searchResults.length).toBe(1)
+  });
 })
